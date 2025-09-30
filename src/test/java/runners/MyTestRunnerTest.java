eaa5c1af1dd58af1cf8300f1939512af635a2607
kamesh2541991 <kameshmuthukumar@gmail.com>
2025-09-30 22:20:35 +0530

    Checking GITHUB actions

--- src/test/java/runners/MyTestRunnerTest.java
+++ src/test/java/runners/MyTestRunnerTest.java
@@ -7,6 +7,8 @@ import org.junit.platform.suite.api.ConfigurationParameter;
 @Suite
 @SelectPackages("features")
 @ConfigurationParameter(key = "glue", value = "stepdefinitions; hooks")
+@ConfigurationParameter(key = "tags", value = "Logout")
 @ConfigurationParameter(key = "plugin", value = "pretty,html:target/cucumber-reports.html,json:target/cucumber-reports.json,com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:")
 public class MyTestRunnerTest {
+
 }
