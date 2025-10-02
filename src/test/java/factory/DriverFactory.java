152276537a40e00bbc942826a73d4985336b340c
kamesh2541991 <kameshmuthukumar@gmail.com>
2025-10-03 00:11:53 +0530

    removed chromeoptions

--- src/test/java/factory/DriverFactory.java
+++ src/test/java/factory/DriverFactory.java
@@ -14,26 +14,25 @@ public class DriverFactory {
     public static WebDriver getDriver() {
         if (driver == null) {
             WebDriverManager.chromedriver().setup();
-
-            ChromeOptions options = new ChromeOptions();
-
-            //options.addArguments("--headless=new"); // Use new headless mode
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--disable-gpu");
-            options.addArguments("--remote-allow-origins=*");
-
-            // ✅ Add a unique user-data-dir to avoid session conflicts
-            try {
-                Path tempProfile = Files.createTempDirectory("chrome-profile");
-                options.addArguments("--user-data-dir=" + tempProfile.toAbsolutePath().toString());
-            } catch (IOException e) {
-                throw new RuntimeException("Failed to create temp user-data-dir", e);
-            }
-
-            driver =  new ChromeDriver(options);
+//
+//            ChromeOptions options = new ChromeOptions();
+//
+//            //options.addArguments("--headless=new"); // Use new headless mode
+//            options.addArguments("--no-sandbox");
+//            options.addArguments("--disable-dev-shm-usage");
+//            options.addArguments("--disable-gpu");
+//            options.addArguments("--remote-allow-origins=*");
+//
+//            try {
+//                Path tempProfile = Files.createTempDirectory("chrome-profile");
+//                options.addArguments("--user-data-dir=" + tempProfile.toAbsolutePath().toString());
+//            } catch (IOException e) {
+//                throw new RuntimeException("Failed to create temp user-data-dir", e);
+//            }
+//
+//            driver =  new ChromeDriver(options);
             //System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/java/drivers/chromedriver.exe");
-            //driver = new ChromeDriver();
+            driver = new ChromeDriver();
         }
         return driver;
     }
